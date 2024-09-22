5b5000f54ed28936b247d92e0cc127fa7acb8ec7
Acidv1p3r <50040279+acidv1p3r@users.noreply.github.com>
2024-09-22 16:27:52 -0400

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -16,51 +16,38 @@ function Test-InternetConnection {
     }
 }
 
-# Function to install Nerd Fonts
-function Install-NerdFonts {
-    param (
-        [string]$FontName = "CascadiaCode",
-        [string]$FontDisplayName = "CaskaydiaCove NF",
-        [string]$Version = "3.2.1"
-    )
-
-    try {
-        [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
-        $fontFamilies = (New-Object System.Drawing.Text.InstalledFontCollection).Families.Name
-        if ($fontFamilies -notcontains "${FontDisplayName}") {
-            $fontZipUrl = "https://github.com/ryanoasis/nerd-fonts/releases/download/v${Version}/${FontName}.zip"
-            $zipFilePath = "$env:TEMP\${FontName}.zip"
-            $extractPath = "$env:TEMP\${FontName}"
-
-            $webClient = New-Object System.Net.WebClient
-            $webClient.DownloadFileAsync((New-Object System.Uri($fontZipUrl)), $zipFilePath)
-
-            while ($webClient.IsBusy) {
-                Start-Sleep -Seconds 2
-            }
-
-            Expand-Archive -Path $zipFilePath -DestinationPath $extractPath -Force
-            $destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
-            Get-ChildItem -Path $extractPath -Recurse -Filter "*.ttf" | ForEach-Object {
-                If (-not(Test-Path "C:\Windows\Fonts\$($_.Name)")) {
-                    $destination.CopyHere($_.FullName, 0x10)
-                }
-            }
-
-            Remove-Item -Path $extractPath -Recurse -Force
-            Remove-Item -Path $zipFilePath -Force
-        } else {
-            Write-Host "Font ${FontDisplayName} already installed"
-        }
-    }
-    catch {
-        Write-Error "Failed to download or install ${FontDisplayName} font. Error: $_"
-    }
-}
-
 # Check for internet connectivity before proceeding
 if (-not (Test-InternetConnection)) {
     break
+}
+
+# Install Oh My Posh
+try {
+    winget install -e --accept-source-agreements --accept-package-agreements JanDeDobbeleer.OhMyPosh
+}
+catch {
+    Write-Error "Failed to install Oh My Posh. Error: $_"
+}
+
+# Install Nerd Fonts
+Install-NerdFonts -FontName "CascadiaCode" -FontDisplayName "CaskaydiaCove NF"
+
+# Chocolatey installation
+try {
+    Set-ExecutionPolicy Bypass -Scope Process -Force; 
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
+    iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+catch {
+    Write-Error "Failed to install Chocolatey. Error: $_"
+}
+
+# Install FastFetch
+try {
+    choco install fastfetch -y
+}
+catch {
+    Write-Error "Failed to install FastFetch. Error: $_"
 }
 
 # Profile creation or update
@@ -89,36 +76,22 @@ if (!(Test-Path -Path $PROFILE -PathType Leaf)) {
 }
 else {
     try {
-        Get-Item -Path $PROFILE | Move-Item -Destination "oldprofile.ps1" -Force
-        Invoke-RestMethod https://github.com/acidv1p3r/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -OutFile $PROFILE
-        Write-Host "The profile @ [$PROFILE] has been created and old profile removed."
-        Write-Host "Please back up any persistent components of your old profile to [$HOME\Documents\PowerShell\Profile.ps1] as there is an updater in the installed profile which uses the hash to update the profile and will lead to loss of changes."
+        $oldProfilePath = "C:\Users\Administator\Profile.ps1"
+        $useOldProfile = Read-Host "An old profile was found. Do you want to use it located at [$oldProfilePath]? (Y/N)"
+
+        if ($useOldProfile -eq 'Y' -or $useOldProfile -eq 'y') {
+            Copy-Item -Path $oldProfilePath -Destination $PROFILE -Force
+            Write-Host "Old profile has been copied to [$PROFILE]."
+        } else {
+            Get-Item -Path $PROFILE | Move-Item -Destination "oldprofile.ps1" -Force
+            Invoke-RestMethod https://github.com/acidv1p3r/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -OutFile $PROFILE
+            Write-Host "The profile @ [$PROFILE] has been created and old profile removed."
+            Write-Host "Please back up any persistent components of your old profile to [$HOME\Documents\PowerShell\Profile.ps1] as there is an updater in the installed profile which uses the hash to update the profile and will lead to loss of changes."
+        }
     }
     catch {
         Write-Error "Failed to backup and update the profile. Error: $_"
     }
-}
-
-# OMP Install
-try {
-    winget install -e --accept-source-agreements --accept-package-agreements JanDeDobbeleer.OhMyPosh
-}
-catch {
-    Write-Error "Failed to install Oh My Posh. Error: $_"
-}
-
-# Font Install
-Install-NerdFonts -FontName "CascadiaCode" -FontDisplayName "CaskaydiaCove NF"
-
-# Choco install
-try {
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-    
-    # Install FastFetch via Chocolatey
-    choco install fastfetch -y
-}
-catch {
-    Write-Error "Failed to install Chocolatey or FastFetch. Error: $_"
 }
 
 # Final check and message to the user
